5593103dcd3bbe4f3ed75603fb8355f42d352b23
sayed-kassem <sayed.webdev@gmail.com>
2025-04-23 12:54:19 +0300

    Initial Commit

---
+++ src/content_option.js
@@ -0,0 +1,174 @@
+const logotext = "SAYED";
+const meta = {
+    title: "Sayed",
+    description: "I’m Sayed _ web devloper,currently working in Lebanon",
+};
+
+const introdata = {
+    title: "I’m Sayed",
+    animated: {
+        first: "Web Developer",
+        second: "Creating Websites",
+        third: "Developing Mobile Apps",
+    },
+    description: "computer engineer graduate with passion for software.",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "enthuastic into translating designs into actual functional websites using the latest technologies like React, TailwindCSS and other.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Data Analyst - IT",
+        where: "School - Unicef",
+        date: "2024",
+    }
+    ,{
+        jobtitle: "Data Engineer",
+        where: "Dubai",
+        date: "2022",
+    },
+    {
+        jobtitle: "Freelance",
+        where: "Remote",
+        date: "From 2022",
+    },
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 85,
+    },
+    {
+        name: "Css",
+        value: 80,
+    },
+    {
+        name: "Figma",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 70,
+    },
+    {
+        name: "TypeScript",
+        value: 85
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Creating wirefireames & prototypes with Figma. Upholding to the latest practices and design principles.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Developing mobile apps with native Android Studio & Kotlin, Jetpack Compose and SDK",
+    },
+    {
+        title: "Web Development",
+        description: "Building Web Apps and Sites with latest version of React, TailwindCSS and latest React Patterns",
+    },
+];
+
+const dataportfolio = [{
+        img: "/music.png",
+        description: "vueJs music player web app",
+        link: "https://soundscape-nu.vercel.app/",
+    },
+    {
+        img: "https://lebanesefestival.ca/wp-content/uploads/2024/06/Lebfest-Logo-1536x781.png",
+        description: "WordPress website for LebaneseFestival in Halifax,Ca",
+        link: "LebaneseFestival.ca",
+    },
+    // {
+    //     img: "https://picsum.photos/400/800/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "sayed.kassem@proton.me",
+    YOUR_FONE: "+961-71844608",
+    description: "",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_ufkiqt5",
+    YOUR_TEMPLATE_ID: "template_bne4spe",
+    YOUR_USER_ID: "ZUGdKfr4Hcx604cMF",
+};
+
+const socialprofils = {
+    github: "https://github.com/sayed-kassem",
+    linkedin: "https://linkedin.com/in/sayed-kassem",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/contact/index.js
@@ -0,0 +1,171 @@
+import React, { useState } from "react";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../../content_option";
+import { Container, Row, Col, Alert } from "react-bootstrap";
+import { contactConfig } from "../../content_option";
+
+export const ContactUs = () => {
+
+  const formUrl = process.env.REACT_APP_URL_EMAIL_FORM;
+  const [formData, setFormdata] = useState({
+    email: "",
+    name: "",
+    message: "",
+    loading: false,
+    show: false,
+    alertmessage: "",
+    variant: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(process.env.REACT_APP_URL_EMAIL_FORM)
+  setFormdata({ ...formData, loading: true });
+
+  fetch(formUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: formData.name,
+      email: formData.email,
+      message: formData.message,
+    }),
+  })
+    .then((res) => {
+      if (res.ok) {
+        setFormdata({
+          ...formData,
+          name: "",
+          email: "",
+          message: "",
+          loading: false,
+          alertmessage: "SUCCESS! Thank you for your message.",
+          variant: "success",
+          show: true,
+        });
+      } else {
+        throw new Error("Form submission failed");
+      }
+    })
+    .catch((err) => {
+      setFormdata({
+        ...formData,
+        loading: false,
+        alertmessage: `Failed to send! ${err.message}`,
+        variant: "danger",
+        show: true,
+      });
+      document.getElementsByClassName("co_alert")[0].scrollIntoView();
+    });
+  };
+
+  const handleChange = (e) => {
+    setFormdata({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <HelmetProvider>
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Contact</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">Contact Me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="12">
+            <Alert
+              variant={formData.variant}
+              className={`rounded-0 co_alert ${
+                formData.show ? "d-block" : "d-none"
+              }`}
+              onClose={() =>
+                setFormdata({ ...formData, show: false, alertmessage: "" })
+              }
+              dismissible
+            >
+              <p className="my-0">{formData.alertmessage}</p>
+            </Alert>
+          </Col>
+          <Col lg="5" className="mb-5">
+            <h3 className="color_sec py-4">Get in touch</h3>
+            <address>
+              <strong>Email:</strong>{" "}
+              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
+                {contactConfig.YOUR_EMAIL}
+              </a>
+              <br />
+              <br />
+              {contactConfig.YOUR_FONE && (
+                <p>
+                  <strong>Phone:</strong> {contactConfig.YOUR_FONE}
+                </p>
+              )}
+            </address>
+            <p>{contactConfig.description}</p>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <form onSubmit={handleSubmit} className="contact__form w-100">
+              <Row>
+                <Col lg="6" className="form-group">
+                  <input
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    placeholder="Name"
+                    value={formData.name}
+                    type="text"
+                    required
+                    onChange={handleChange}
+                  />
+                </Col>
+                <Col lg="6" className="form-group">
+                  <input
+                    className="form-control rounded-0"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    type="email"
+                    value={formData.email}
+                    required
+                    onChange={handleChange}
+                  />
+                </Col>
+              </Row>
+              <textarea
+                className="form-control rounded-0"
+                id="message"
+                name="message"
+                placeholder="Message"
+                rows="5"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+              <br />
+              <Row>
+                <Col lg="12" className="form-group">
+                  <button className="btn ac_btn" type="submit">
+                    {formData.loading ? "Sending..." : "Send"}
+                  </button>
+                </Col>
+              </Row>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+      <div className={formData.loading ? "loading-bar" : "d-none"}></div>
+    </HelmetProvider>
+  );
+};
